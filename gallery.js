73beeef68a7d2a672034a9ab68aa7f279be8de8c
Nadya <Nadysha>
2023-02-17 20:45:37 +0200

    10week

---
+++ gallery.js
@@ -0,0 +1,9 @@
+function showImage() {
+    let image = document.getElementById('cat');
+    image.src = "/image/cat2.jpg";
+}
+
+function showImage2() {
+    let image = document.getElementById('cat');
+    image.src = "/image/cat1.jpg";
+}
